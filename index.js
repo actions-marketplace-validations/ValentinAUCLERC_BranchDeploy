27e29328e01d1d8e69365749d2860e54ba55c58d
Valentin <valentin.auclerc@gmail.com>
2024-02-18 22:34:50 +0100

    test

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const main = async() => {
         });
 
         console.log(github.context);
-        if(github.context.comment.body.match('/\\.deploy\\s+(.+)/')) {
+        if(github.context.payload.comment.body.match('/\\.deploy\\s+(.+)/')) {
             octokit.rest.issues.createComment({
                 owner,
                 repo,
