475c21ecfc567d92ad878c7b4ab4d54f3f50de08
Valentin <valentin.auclerc@gmail.com>
2024-02-18 23:47:07 +0100

    test

--- index.js
+++ index.js
@@ -17,52 +17,48 @@ const main = async() => {
         const issue_number = github.context.payload.issue.number;
         const {owner, repo} = github.context.repo;
 
+        function createComment(body) {
+            octokit.rest.issues.createComment({
+                owner,
+                repo,
+                issue_number: issue_number,
+                body: body
+            });
+        }
+
         const pr = await octokit.rest.pulls.get({
             owner: owner,
             repo: repo,
             pull_number: issue_number
         });
 
-
-        if(github.context.payload.comment.body.match('/\.deploy\s+(.+)/')) {
-            octokit.rest.issues.createComment({
-                owner,
-                repo,
-                issue_number: issue_number,
-                body:
-`### Deployment Triggered 🚀
-__${github.context.actor}__, started a deployment to SSH !
-You can watch the progress [here](https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/actions/runs/${process.env.GITHUB_RUN_ID}) 🔗
-> Branch: \`${pr?.data?.base?.ref}\``
-            });
-
-            const conn = new Client();
-            conn.on('ready', () => {
-                console.log('SSH client :: ready');
-                conn.exec(script, (err, stream) => {
-                    if (err) throw err;
-                    stream.on('data', (data) => {
-                        console.log('STDOUT: ' + data);
-                        octokit.rest.issues.createComment({
-                            owner,
-                            repo,
-                            issue_number: issue_number,
-                            body: `ceci
-                        est un test`
+        var commandPattern = /^\.deploy\s*/;
+        var triggerComment = github.context.payload.comment.body;
+        if(commandPattern.test(triggerComment)) {
+            var paramString = triggerComment.replace(commandPattern, '');
+            if(/(-\w* \S*)\s*/g.test(paramString) === false && paramString !== '') {
+                createComment('👮 Due to security policy, blabla')
+            } else {
+                const conn = new Client();
+                conn.on('ready', () => {
+                    console.log('SSH client :: ready');
+                    conn.exec(script+' '+paramString, (err, stream) => {
+                        if (err) throw err;
+                        stream.on('data', (data) => {
+                            console.log('STDOUT: ' + data);
+                            createComment(data);
+                        }).on('close', (code) => {
+                            console.log('stream :: close\n', { code });
+                            conn.end();
                         });
-                    }).on('close', (code) => {
-                        console.log('stream :: close\n', { code });
-                        conn.end();
                     });
+                }).connect({
+                    host: host,
+                    port: port,
+                    username: username,
+                    password: password
                 });
-            }).connect({
-                host: host,
-                port: port,
-                username: username,
-                password: password
-            });
-        } else {
-            console.log(github.context.payload.comment.body);
+            }
         }
 
 
